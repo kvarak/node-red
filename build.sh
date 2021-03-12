2551ed747becb7509ea8e8f9d7baddebddb325fd
Albert Rigo <albert.rigo@gmail.com>
2021-03-12 19:03:42 +0100

    Adds version to docker hub

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash -x
 
-docker build --tag kvarak/node-red:latest .
-docker push kvarak/node-red
+VERSION=0.1
+
+if [ "$1" == "build" ] ; then
+    docker build -t kvarak/node-red:latest -t kvarak/node-red:$VERSION .
+elif [ "$1" == "push" ] ; then
+    docker push kvarak/node-red:latest
+    docker push kvarak/node-red:$VERSION
+else
+    echo "Choose build or push"
+fi
